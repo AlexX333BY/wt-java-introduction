0dfeabdc78acd37904f75eebc5cb1fda8042623e
Aliaksei <alexx333by@pavilion-dv6.benis.design>
2018-10-08 22:02:04 +0300

    Removed tangent test because Math.tan(PI / 2) isn't specified to return inf

--- test/by/bsuir/kaziukovich/task3/tangent/StepTangentCounterJUnitTest.java
+++ test/by/bsuir/kaziukovich/task3/tangent/StepTangentCounterJUnitTest.java
@@ -18,9 +18,6 @@ class StepTangentCounterJUnitTest {
         /* tg(0) == 0 */
         Assertions.assertEquals(new Double(0), StepTangentCounter.count(0, 0, 0.1).get((double) 0));
 
-        /* tg(PI / 2) == Inf */
-        Assertions.assertEquals(new Double(Double.POSITIVE_INFINITY), StepTangentCounter.count(Math.PI / 2, Math.PI / 2, 0.1).get(Math.PI / 2));
-
         Assertions.assertThrows(IllegalArgumentException.class, () -> StepTangentCounter.count(1, -1, 0.1));
         Assertions.assertThrows(IllegalArgumentException.class, () -> StepTangentCounter.count(-1, 1, -0.1));
     }
